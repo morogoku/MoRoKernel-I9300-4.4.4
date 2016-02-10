81183dc64ecde3778d391f2f2646a6d6380f993b
morogoku <morogoku@hotmail.com>
2016-02-10 22:04:34 +0100

    MoRoKernel 1.8 Initial commit

--- build_kernel.sh
+++ build_kernel.sh
@@ -23,7 +23,7 @@ TOOLCHAIN_PATCH="/home/moro/kernel/toolchains/arm-eabi-UBER-4.9/bin"
 ROOTFS_PATH="/home/moro/kernel/MoRoKernel-I9300-4.4.4/ramdisk"
 RAMFS_TMP="/home/moro/kernel/tmp/ramfs-source-sgs3"
 
-export KERNEL_VERSION="MoRoKernel-1.7"
+export KERNEL_VERSION="MoRoKernel-1.8"
 export VERSION_KL="GT-I9300"
 export REVISION="RC"
 
